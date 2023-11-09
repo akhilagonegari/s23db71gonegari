a766b5c7acf31ff5f4f3f7f0c862e1a1a0e89032
Akhila Gonegari <142836078+akhilagonegari@users.noreply.github.com>
2023-11-08 22:52:03 -0600

    -/mfileupdated

--- app.js
+++ app.js
@@ -87,3 +87,45 @@ if (reseed) {
 }
  
 module.exports = app;
+
+exports.dogs_create_post = async function(req, res) {
+console.log(req.body)
+let document = new dogs();
+// We are looking for a body, since POST does not have query parameters.
+// Even though bodies can be in many different formats, we will be picky
+// and require that it be a json object
+// {"costume_type":"goat", "cost":12, "size":"large"}
+document.dogs_type = req.body.dogs_type;
+document.cost = req.body.cost;
+document.size = req.body.size;
+try{
+let result = await document.save();
+res.send(result);
+}
+catch(err){
+res.status(500);
+res.send(`{"error": ${err}}`);
+}
+ 
+};
+// Handle Costume delete form on DELETE.
+exports.dogs_delete = function(req, res) {
+res.send('NOT IMPLEMENTED: dogs delete DELETE ' + req.params.id);
+};
+// Handle Costume update form on PUT.
+exports.dogs_update_put = function(req, res) {
+res.send('NOT IMPLEMENTED: dogs update PUT' + req.params.id);
+};
+ 
+// VIEWS
+// Handle a show all view
+exports.dogs_view_all_Page = async function(req, res) {
+    try{
+    thedogs = await dogs.find();
+    res.render('dogs', { title: 'Costume Search Results', results: thedogs });
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    };
